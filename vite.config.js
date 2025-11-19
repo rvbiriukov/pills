35a3f112c39e53d85336d406528b7c12f9bdd68a
Roman Biriukov <rvbiriukov@gmail.com>
2025-11-19 17:33:44 +0000

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // IMPORTANT: If deploying to https://<USERNAME>.github.io/<REPO_NAME>/
   // Set base to '/<REPO_NAME>/' (e.g., base: '/pills/')
-  // base: '/pills/', 
+  base: '/pills/',
 })
